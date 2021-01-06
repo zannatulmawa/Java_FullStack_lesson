6219d22bfac73ec6bafb9b02631aa3824495c80d
Mahbub <mahbub@udel.edu>
2021-01-06 15:58:36 -0500

    updated L06

--- L06_Big_Decimal/src/Bdecimal.java
+++ L06_Big_Decimal/src/Bdecimal.java
@@ -7,9 +7,9 @@ public class Bdecimal {
 		
 		double x = 1.05;
 		double y = 2.55;
-		BigDecimal d1 = new BigDecimal(1.05);
-		BigDecimal d2 = new BigDecimal(2.55);
-		System.out.println(x+y);
+		BigDecimal d1 = new BigDecimal("1.05");
+		BigDecimal d2 = new BigDecimal("2.55");
+		System.out.println(d1.add(d2));
 		
 
 	}
