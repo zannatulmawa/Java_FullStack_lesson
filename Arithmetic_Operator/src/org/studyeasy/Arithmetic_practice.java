f155a3591c6f0e5ab60ba1cdf84d48602f315b63
Mahbub <mahbub@udel.edu>
2021-01-11 16:44:17 -0500

    Two more lesson added

---
+++ Arithmetic_Operator/src/org/studyeasy/Arithmetic_practice.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+
+public class Arithmetic_practice {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 12%2;
+		x++;
+		System.out.println(x);
+
+	}
+
+}
---
+++ L09_Operators/src/org/studyeasy/Operators_lesson.java
@@ -0,0 +1,18 @@
+package org.studyeasy;
+
+public class Operators_lesson {
+
+	public static void main(String[] args) {
+		int ageofBoy = 41;
+		int ageofGirl = 32;
+		
+		if ((ageofBoy >= 55) && (ageofGirl >= 24)){
+			System.out.println("Ready to get married");
+		}
+		
+		else {
+		System.out.println("This is not the time");
+		}
+
+	}
+}
