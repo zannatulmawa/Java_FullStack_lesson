acf9b13d3a061183df6ffbbd89b5f9b38d7d80df
Mahbub <mahbub@udel.edu>
2021-01-11 10:57:19 -0500

    recently updated

---
+++ Exercise_1/src/org/studyeasy/Exercise.java
@@ -0,0 +1,10 @@
+package org.studyeasy;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- L08_Type_Casting/src/org/studyeasy/Hello.java
+++ L08_Type_Casting/src/org/studyeasy/Hello.java
@@ -4,26 +4,15 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		/*
-		short y = 23; 
-		int x = y;
-		*/
-		
+		//explicit: backward
 		int y = 10;
 		//short x = y;  // it won't work
 		//because Implicit type casting (widening) byte-short-int-long-float-double
 		
 		short x = (short)y; //short or byte will work because they require less memory according to the implicit casting
-		
-				
-		
 		System.out.println(y);
 		
-		/*
-		float v1 = 10.0f;
-		double v2 = v1;
-		*/
-		
+
 		double v2 = 20.8123456789021d;
 		float v1 = (float) v2; //Explicit type Casting
 		
@@ -46,10 +35,21 @@ public class Hello {
 		System.out.println(g); //It will show an error
 		*/
 		
+		//explicit casting: forward
+		//trying out int-->float
+		int u = 5;
+		double u_float = (double) u;
+		System.out.println("u_float:"+ u_float);
+		
 		
 		float g = 10.0f;
 		long l = (long)g;
 		System.out.println(g); //It will work as we converted long to long
+		
+		//implicit casting
+		short s_a = 3;
+		float short_float = s_a;
+		System.out.println("short a: "+ short_float);
 
 	}
 
