9dcb5d2bce95730b57475fa46dbefd9187da4cd5
Mahbub <mahbub@udel.edu>
2021-01-11 11:24:57 -0500

    exercise 1 added

--- Exercise_1/src/org/studyeasy/Exercise.java
+++ Exercise_1/src/org/studyeasy/Exercise.java
@@ -3,7 +3,11 @@ package org.studyeasy;
 public class Exercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 12;
+		float b = 23.567f;
+		double value = a*a + 2(a*b) + b*b;
+		System.out.println(value);
 
 	}
 
---
+++ Exercise_1/src/org/studyeasy/Hello.java
@@ -0,0 +1,13 @@
+package org.studyeasy;
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		int a = 12;
+		float b = 23.567f;
+		//double value = a*a + 2*(a*b) + b*b;
+		int value = (int)(a*a + 2*(a*b) + b*b);
+		System.out.println("The value is : " + value);
+
+}
+}
