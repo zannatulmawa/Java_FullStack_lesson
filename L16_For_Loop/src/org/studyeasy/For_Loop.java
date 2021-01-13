a4d66ff136f257f71d4b376ee6e3a9744c3ad4e8
Mahbub <mahbub@udel.edu>
2021-01-13 14:32:01 -0500

    new Addede

---
+++ L16_For_Loop/src/org/studyeasy/For_Loop.java
@@ -0,0 +1,26 @@
+package org.studyeasy;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		
+		/*
+		for (int i = 1; i<=10; i++)
+		System.out.println(i);
+		*/
+		
+		
+		int primeNum = 19 ;
+		
+		//for(int i = 1; i<=10; i++)
+			if(primeNum % 2 == 0) {
+				System.out.println("This is not a prime number");	
+			}
+			else {
+				System.out.println("This is a prime number");
+			}
+			
+		
+	}
+
+}
---
+++ L17_While_Loop/src/org/studyeasy/While_Loop.java
@@ -0,0 +1,15 @@
+package org.studyeasy;
+
+public class While_Loop {
+
+	public static void main(String[] args) {
+		int i =1;
+		while(i<=10) {
+			System.out.println("Hello");
+			i++;
+		}
+		
+
+	}
+
+}
