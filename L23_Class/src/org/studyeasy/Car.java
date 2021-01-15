3ccbf71bbf98e171399a868c8634d52a422256b3
Mahbub <mahbub@udel.edu>
2021-01-15 17:35:01 -0500

    class example added

---
+++ L23_Class/src/org/studyeasy/Car.java
@@ -0,0 +1,16 @@
+package org.studyeasy;
+
+public class Car {
+
+	private String doors; //private is a method
+	private String engine;
+	private String driver;
+	private int speed;
+	
+	public void setSpeed(int speed) { //method
+		this.speed = speed;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+}
---
+++ L23_Class/src/org/studyeasy/Class_Ex1.java
@@ -0,0 +1,15 @@
+package org.studyeasy;
+
+public class Class_Ex1 {
+
+	public static void main(String[] args) {
+		Car car = new Car(); //Object of car class (Crating an object)
+				//new Car (This is the way of creating custom data type, here Car is the constructor)
+		
+		
+		car.setSpeed(10);
+		System.out.println(car.getSpeed());
+
+	}
+
+}
