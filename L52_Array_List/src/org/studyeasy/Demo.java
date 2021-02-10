b44e80b88e21d13b4c3b0edcf89a254e28021130
Mahbub <mahbub@udel.edu>
2021-02-10 13:32:37 -0500

    updated

--- L52_Array_List/src/org/studyeasy/Demo.java
+++ L52_Array_List/src/org/studyeasy/Demo.java
@@ -1,10 +1,47 @@
 package org.studyeasy;
 
+import java.util.ArrayList;
+
 public class Demo {
-
+	
+	
+	static ArrayList<String> listNames = new ArrayList<>();
+	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		listNames.add("Mawa");
+		listNames.add("Bony");
+		listNames.add("Ridwone");
+		listNames.add("Ananna");
+		listNames.add("Chamaly");
+		listNames.add("Amzad");
+		listNames.add("Morjina");
+		//System.out.println(listNames);
+		//System.out.println(listNames.get(1));
+		
+		Demo demoObj = new Demo();
+		demoObj.displayList(listNames);
+		demoObj.removeNameByPosition(0);
+		System.out.println("***************");
+		demoObj.displayList(listNames);
+		
+		
+		System.out.println("***************");
+		Demo nameObj = new Demo();
+		nameObj.removeNameByName("Morjina");
+		nameObj.displayList(listNames);
 
+	}
+	void displayList(ArrayList<String> names) {
+		for(String name: names) {
+			System.out.println(name);
+		}
+	}
+	
+	void removeNameByPosition(int position) {
+		listNames.remove(position);
+	}
+	void removeNameByName(String name) {
+		listNames.remove(name);
 	}
 
 }
